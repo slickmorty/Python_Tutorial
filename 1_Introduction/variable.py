0888919f3cd4103eb1e3d51578fba168ad4467e5
Ahmad Abasi <a_abasi97@yahoo.com>
2023-07-05 17:24:24 +0330

    Started Again :)

--- 1_Introduction/variable.py
+++ 1_Introduction/variable.py
@@ -1,8 +1,11 @@
 x: int = 10
-y: int = 20
+y: float = 2.1
 name_1: str = "Amin"
 name_2: str = "Amir"
 # variable (: type) = value
+is_okey: bool = True
 
 print(x + y)
 print(name_1 + name_2)
+print(x * name_1)
+# print(x + name_1) ERROR can't add str with int
